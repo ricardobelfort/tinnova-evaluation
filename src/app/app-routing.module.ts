9f017e7856104ed56890e8d46da0255b9301d2e6
Ricardo Belfort <rbelfort2004@gmail.com>
2022-08-07 18:45:15 -0400

    toolbar, módulo de usuários e roteamento

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'usuarios'
+  },
+  {
+    path: 'usuarios',
+    loadChildren: () => import('./usuarios/usuarios.module').then(m => m.UsuariosModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
